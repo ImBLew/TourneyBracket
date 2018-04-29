723d2cb9f38d8c741d6b0762742ce559e0f7452b
ImBlew <bslewis2@wisc.edu>
2018-04-29 14:17:10 -0500

    addToLayout and Button added

--- src/application/Match.java
+++ src/application/Match.java
@@ -1,9 +1,9 @@
 package application;
 
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class Match{
 
@@ -63,17 +63,36 @@ public class Match{
         scoreButton = new Button();
         scoreButton.setText("Final Score");
         scoreButton.setLayoutX(x + x/3.0);
+        scoreButton.setMaxWidth(width/3.0);
+        scoreButton.setLayoutY(y + y/3.0);
+        scoreButton.setMaxHeight(height/3.0);
+        
+        /* *************** IS LEFT HANDLING *************** */
+        if (!isLeft) {
+            double tmp = team1Label.getLayoutX();
+            team1Label.setLayoutX(team1TextField.getLayoutX());
+            team1TextField.setLayoutX(tmp);
+            
+            tmp = team2Label.getLayoutX();
+            team2Label.setLayoutX(team2TextField.getLayoutX());
+            team2TextField.setLayoutX(tmp);
+        }
     }
     
     public Match(int x, int y, int width, int height, boolean isLeft, Team team1, Team team2) {
         this(x,y,width,height,isLeft);
+        
         this.team1 = team1;
         team1Label.setText(team1.getName());
         this.team2 = team2;
         team2Label.setText(team2.getName());
     }
     
-    public void addToScene(Scene s) {
-        
+    public void addToLayout(GridPane grid) {
+        grid.add(team1Label, (int)team1Label.getLayoutY(), (int)team1Label.getLayoutX());
+        grid.add(team2Label, (int)team2Label.getLayoutY(), (int)team2Label.getLayoutX());
+        grid.add(team1TextField, (int)team1TextField.getLayoutX(), (int)team1TextField.getLayoutY());
+        grid.add(team2TextField, (int)team2TextField.getLayoutX(), (int)team2TextField.getLayoutY());
+        grid.add(scoreButton, (int)scoreButton.getLayoutX(), (int)scoreButton.getLayoutY());
     }
 }
